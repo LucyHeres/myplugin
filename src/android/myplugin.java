dce365771c3fff49ce518243fe5bffb816dcde15
LucyHeres <liuxinat126@126.com>
2017-11-06 15:08:12 +0800

    Update myplugin.java

--- src/android/myplugin.java
+++ src/android/myplugin.java
@@ -1,4 +1,4 @@
-package cordova.plugins.myplugin;
+package cordova.plugin.myplugin;
 
 import android.widget.Toast;
 
@@ -16,18 +16,8 @@ public class myplugin extends CordovaPlugin {
 
         if ("toast".equals(action)) {
             String str = args.getString(0);
+            System.out.print("myplugin");
             Toast.makeText(cordova.getActivity(), str, Toast.LENGTH_LONG).show();
-            return true;
-        } else if ("toastWithCallback".equals(action)) {
-            String str = args.getString(0);
-            Toast.makeText(cordova.getActivity(), str, Toast.LENGTH_LONG).show();
-            //为体现成功／失败两种回调，这里使用Random生成随机数，随机数为true时回调成功function，为false时回调失败function，
-            Random random = new Random();
-            if (random.nextBoolean()) {
-                callbackContext.success("execute_callack_success");
-            } else {
-                callbackContext.error("execute_callack_error");
-            }
             return true;
         }
         return super.execute(action, args, callbackContext);
